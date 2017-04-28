21d54a8bacf8056929f86a61f0f212783d5f231a
oikawa <oikawa@chiba-fjb.ac.jp>
2017-04-28 11:56:16 +0900

    四則演算作成2

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,25 @@ namespace Example2
         {
             InitializeComponent();
         }
+
+        private void btnCalcAdd_Click(object sender, EventArgs e)
+        {
+            lblAnsAdd.Text = String.Format("{0}", 1 + 2);
+        }
+
+        private void btnCalSub_Click(object sender, EventArgs e)
+        {
+            lblAnsSub.Text = String.Format("{0}", 5 - 3);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lblAnsMul.Text = String.Format("{0}", 2 * 3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lblAnsDiv.Text = String.Format("{0}", 8/2);
+        }
     }
 }
